6477b16115f4f8067c2dd2c24f4f010d879809c8
Dipak1121 <91940812+Dipak1121@users.noreply.github.com>
2023-11-29 21:17:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 // write js code here if required
+let p = document.getElementById("timer");
+setInterval(printDate, 1000);
+function printDate(){
+	let dateObj = new Date();
+	let date = dateObj.getDate();
+	let month = dateObj.getMonth();
+	let year = dateObj.getFullYear();
+	let hour = dateObj.getHours();
+	let minute = dateObj.getMinutes();
+	let second = dateObj.getSeconds();
+	let dateStr = `${date}/${month}/${year}, ${hour}:${minute}:${second}`;
+	p.innerText = dateStr;
+}
